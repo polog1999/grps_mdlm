a6206108ea4db74af5975597a9bc6b053754baca
diegoAcuna1612 <u20201c794@upc.edu.pe>
2025-12-02 09:38:36 -0500

    feat: al actualizar un certificado en la vista editar, este vuelve al index, y ademas se actualiza el certificado original

--- app/Filament/Clusters/Sil/Resources/CertificadoInspeccionResource/Pages/EditCertificadoInspeccion.php
+++ app/Filament/Clusters/Sil/Resources/CertificadoInspeccionResource/Pages/EditCertificadoInspeccion.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Clusters\Sil\Resources\CertificadoInspeccionResource\Pages;
 
 use App\Filament\Clusters\Sil\Resources\CertificadoInspeccionResource\CertificadoInspeccionResource;
+use App\Services\Sil\CertificadoInspeccion\CertificadoPdfGenerator;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
 
 class EditCertificadoInspeccion extends EditRecord
 {
@@ -17,5 +19,37 @@ class EditCertificadoInspeccion extends EditRecord
             ViewAction::make(),
             //DeleteAction::make(),
         ];
+    }
+
+    /**
+     * Redirige a la vista de la tabla después de guardar los cambios.
+     */
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    /**
+     * Hook ejecutado después de guardar el registro.
+     * Regenera automáticamente el PDF del certificado en la carpeta original.
+     */
+    protected function afterSave(): void
+    {
+        $pdfGenerator = app(CertificadoPdfGenerator::class);
+        $filename = $pdfGenerator->generateAndSave($this->record);
+
+        if ($filename) {
+            Notification::make()
+                ->title('PDF Regenerado')
+                ->body("El certificado PDF se regeneró exitosamente: {$filename}")
+                ->success()
+                ->send();
+        } else {
+            Notification::make()
+                ->title('Advertencia')
+                ->body('Los cambios se guardaron pero no se pudo regenerar el PDF automáticamente.')
+                ->warning()
+                ->send();
+        }
     }
 }
