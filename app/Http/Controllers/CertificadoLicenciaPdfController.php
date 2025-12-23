e4649841f7a1195d690011e8c80f3b19917bb39a
diegoAcuna1612 <u20201c794@upc.edu.pe>
2025-12-23 17:08:22 -0500

    feat:formato para pdf certificado de licencia

--- app/Http/Controllers/CertificadoLicenciaPdfController.php
+++ app/Http/Controllers/CertificadoLicenciaPdfController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Services\Sil\Licencias\LicenciaService;
 use App\Services\Sil\Licencias\QrCodeService;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Illuminate\Http\Request;
 
 class CertificadoLicenciaPdfController extends Controller
@@ -18,10 +20,10 @@ class CertificadoLicenciaPdfController extends Controller
     }
 
     /**
-     * Muestra el PDF del certificado de licencia
+     * Genera y muestra el PDF del certificado de licencia
      *
      * @param int $licenciaId
-     * @return \Illuminate\View\View
+     * @return \Illuminate\Http\Response
      */
     public function mostrar(int $licenciaId)
     {
@@ -43,9 +45,83 @@ class CertificadoLicenciaPdfController extends Controller
             ]);
         }
 
-        return view('certificado-licencia', [
+        // Renderizar la vista Blade a HTML
+        $html = view('certificado-licencia', [
             'licencia' => $datosLicencia,
             'qrImage' => $qrDataUri,
+        ])->render();
+
+        // Configurar opciones de Dompdf
+        $options = new Options();
+        $options->set('isRemoteEnabled', true);
+        $options->set('defaultFont', 'Times New Roman');
+        $options->set('isHtml5ParserEnabled', true);
+
+        // Generar PDF
+        $dompdf = new Dompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        $filename = "licencia_funcionamiento_{$licenciaId}.pdf";
+
+        // Mostrar en el navegador
+        return response($dompdf->output(), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => "inline; filename=\"{$filename}\"",
+        ]);
+    }
+
+    /**
+     * Descarga el PDF del certificado de licencia
+     *
+     * @param int $licenciaId
+     * @return \Illuminate\Http\Response
+     */
+    public function descargar(int $licenciaId)
+    {
+        // Obtener datos de la licencia usando el servicio
+        $datosLicencia = $this->licenciaService->obtenerDatosPorIdLicenciaDirecta($licenciaId);
+
+        if (!$datosLicencia) {
+            abort(404, 'Licencia no encontrada');
+        }
+
+        // Intentar obtener el QR code
+        $qrDataUri = null;
+        try {
+            $qrDataUri = $this->qrCodeService->generarQrDataUri($licenciaId);
+        } catch (\Exception $e) {
+            \Log::warning('No se pudo generar QR para licencia', [
+                'licencia_id' => $licenciaId,
+                'error' => $e->getMessage()
+            ]);
+        }
+
+        // Renderizar la vista Blade a HTML
+        $html = view('certificado-licencia', [
+            'licencia' => $datosLicencia,
+            'qrImage' => $qrDataUri,
+        ])->render();
+
+        // Configurar opciones de Dompdf
+        $options = new Options();
+        $options->set('isRemoteEnabled', true);
+        $options->set('defaultFont', 'Times New Roman');
+        $options->set('isHtml5ParserEnabled', true);
+
+        // Generar PDF
+        $dompdf = new Dompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        $filename = "licencia_funcionamiento_{$licenciaId}.pdf";
+
+        // Forzar descarga
+        return response($dompdf->output(), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => "attachment; filename=\"{$filename}\"",
         ]);
     }
 }
