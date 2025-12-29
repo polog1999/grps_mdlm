66efa1bb27e5b76d921bf5f1c110d86d87cbc45b
diegoAcuna1612 <u20201c794@upc.edu.pe>
2025-12-29 15:07:40 -0500

    fix

---
+++ app/Filament/Clusters/Sil/Pages/Auditoria.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Clusters\Sil\Pages;
+
+use App\Filament\Clusters\Sil\SilCluster;
+use Filament\Pages\Page;
+use BackedEnum;
+
+class Auditoria extends Page
+{
+    protected string $view = 'filament.clusters.sil.pages.auditoria';
+
+    protected static ?string $cluster = SilCluster::class;
+
+
+    protected static ?string $recordTitleAttribute = 'Auditoria';
+    protected static ?string $navigationLabel = 'Auditoria';
+    protected static ?string $pluralModelLabel = 'Auditoria';
+
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-document-text';
+
+
+}
