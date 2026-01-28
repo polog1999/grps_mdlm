2f9a9ec4211547e977ce966e8851c919c1503822
diegoAcuna1612 <u20201c794@upc.edu.pe>
2026-01-28 17:10:13 -0500

    refactor: Ajuste en el formulario de Persona en Filament

--- app/Filament/Clusters/Sil/Resources/Personas/Schemas/PersonaForm.php
+++ app/Filament/Clusters/Sil/Resources/Personas/Schemas/PersonaForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Clusters\Sil\Resources\Personas\Schemas;
 
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
@@ -13,15 +14,23 @@ class PersonaForm
         return $schema
             ->components([
                 TextInput::make('per_nombrerazonsocial')
+                    ->label('Nombre razón social')
                     ->required(),
-                TextInput::make('per_ruc'),
-                TextInput::make('per_direccion'),
+                TextInput::make('per_ruc')
+                    ->label('RUC')
+                    ->required(),
+                TextInput::make('per_direccion')
+                    ->label('Dirección')
+                    ->required(),
                 TextInput::make('per_telefono')
+                    ->label('Teléfono')
                     ->tel(),
                 TextInput::make('per_email')
+                    ->label('Email')
                     ->email(),
 
-                TextInput::make('per_expcodcon')
+
+                Hidden::make('per_expcodcon')
                     ->disabled()
                     ->dehydrated(false),
             ]);
