c9200d859628ab3aac8f48e3b38788b866e2e597
polog1999 <polog1999@gmail.com>
2026-02-18 17:14:56 -0500

    NUevos cambios visitantes:probando pide

--- app/Filament/Clusters/Visitas/Resources/Visitas/Tables/VisitasTable.php
+++ app/Filament/Clusters/Visitas/Resources/Visitas/Tables/VisitasTable.php
@@ -16,33 +16,36 @@ class VisitasTable
     public static function configure(Table $table): Table
     {
         return $table
-        ->modifyQueryUsing(fn($query) => $query->leftjoin('visitas.personas as p', 'visitas.visitas.persona_id','=','p.id')->select('visitas.visitas.*'))
-        ->columns([
-            TextColumn::make('persona.numero_documento')->label('N° documento')->searchable()->sortable(),
-            TextColumn::make('persona.full_nombre')->label('Nombres y Apellidos')->searchable(['p.nombres', 'p.apellido_paterno', 'p.apellido_materno']),
-            TextColumn::make('area.nombre')->label('Area'),
-            TextColumn::make('trabajadorAutoriza.persona.full_nombre')->label('Autorizado por'),
-            TextColumn::make('fecha_ingreso')->dateTime('H:i A'),
-            TextColumn::make('fecha_salida')->dateTime('H:i A'),
-            TextColumn::make('motivo'),
-            TextColumn::make('fecha_visita')->badge()
-                ->label('Estado')
-                ->state(fn ($record) => $record->fecha_salida ? 'Salió': 'En Sede')
-                ->color(fn ($record) => $record->fecha_salida ? 'gray' : 'success'),
-        ])
-        ->actions([
-            Action::make('marcar_salida')
-                ->label('Registrar Salida')
-                ->icon('heroicon-o-arrow-right-on-rectangle')
-                ->color('danger')
-                ->visible(fn ($record) => $record->fecha_salida === null) // Solo si no ha salido
-                ->action(function ($record) {
-                    $record->update([
-                        'fecha_salida' => now(),
-                        'user_id_salida' => auth()->id(),
-                    ]);
-                    Notification::make()->title('Salida registrada')->success()->send();
-                })
-        ]);
+            ->modifyQueryUsing(fn($query) => $query->leftjoin('visitas.personas as p', 'visitas.visitas.persona_id', '=', 'p.id')
+            ->select('visitas.visitas.*', 'p.nombres', 'p.apellido_paterno','p.apellido_materno'))
+            ->columns([
+                TextColumn::make('persona.numero_documento')->label('N° documento')->searchable()->sortable(),
+                TextColumn::make('full_nombre')->label('Nombres y Apellidos')->searchable(['p.nombres', 'p.apellido_paterno', 'p.apellido_materno'])
+                    ->formatStateUsing(fn($state, $record) => "{$record->nombres} {$record->apellido_paterno} {$record->apellido_materno}"),
+                TextColumn::make('area.nombre')->label('Area'),
+                TextColumn::make('trabajadorAutoriza.persona.full_nombre')->label('Autorizado por'),
+                TextColumn::make('fecha_ingreso')->dateTime('H:i A'),
+                TextColumn::make('fecha_salida')->dateTime('H:i A'),
+                TextColumn::make('motivo'),
+                TextColumn::make('fecha_visita')->badge()
+                    ->label('Estado')
+                    ->state(fn($record) => $record->fecha_salida ? 'Salió' : 'En Sede')
+                    ->color(fn($record) => $record->fecha_salida ? 'gray' : 'success'),
+            ])
+
+            ->actions([
+                Action::make('marcar_salida')
+                    ->label('Registrar Salida')
+                    ->icon('heroicon-o-arrow-right-on-rectangle')
+                    ->color('danger')
+                    ->visible(fn($record) => $record->fecha_salida === null) // Solo si no ha salido
+                    ->action(function ($record) {
+                        $record->update([
+                            'fecha_salida' => now(),
+                            'user_id_salida' => auth()->id(),
+                        ]);
+                        Notification::make()->title('Salida registrada')->success()->send();
+                    })
+            ]);
     }
 }
