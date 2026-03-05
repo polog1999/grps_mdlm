1bd220db2c82786031a5e7c2e58e432ef7828403
polog1999 <polog1999@gmail.com>
2026-03-05 12:32:36 -0500

    a

--- app/Filament/Clusters/Visitas/Resources/Visitas/Schemas/VisitaForm.php
+++ app/Filament/Clusters/Visitas/Resources/Visitas/Schemas/VisitaForm.php
@@ -89,7 +89,7 @@ class VisitaForm
                                                 ->send();
                                         } else {
                                             $datosApiPeru = PideService::apiPeruDni($state);
-                                            $set('nombres', $datosApiPeru['success']);
+                                           
                                             if ($datosApiPeru['success']) {
                                                 dd('probando');
                                                 $set('pide_fallo', false); // Activamos edición manual
