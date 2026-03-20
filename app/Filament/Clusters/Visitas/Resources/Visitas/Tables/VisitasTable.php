0d44a92f77638707a0c92d29ce88877008ee4beb
polog1999 <polog1999@gmail.com>
2026-03-20 12:39:31 -0500

    a

--- app/Filament/Clusters/Visitas/Resources/Visitas/Tables/VisitasTable.php
+++ app/Filament/Clusters/Visitas/Resources/Visitas/Tables/VisitasTable.php
@@ -174,7 +174,7 @@ class VisitasTable
             $persona = $item->persona;
             
             if ($persona) {
-                return "<b>{$persona->tipoDocumento->nombre_corto}</b> <b>{$persona->numero_documento}</b> <b>{$persona->nombres}</b> <b>{$persona->apellido_paterno}</b> <b>{$persona->apellido_materno}</b> - {$item->cargo}";
+                return "<b>{$persona->tipoDocumento->nombre_corto}</b>&nbsp;<b>{$persona->numero_documento}</b>&nbsp;{$persona->nombres}&nbsp;{$persona->apellido_paterno}&nbsp;{$persona->apellido_materno} - <b>{$item->cargo}</b>";
             }
 
             return "Cargo: {$item->cargo} (Sin datos de persona)";
