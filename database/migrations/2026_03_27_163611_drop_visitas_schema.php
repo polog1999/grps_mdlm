022f7974a04e114b0fc1872796f9b6997b042031
polog1999 <polog1999@gmail.com>
2026-03-30 15:05:41 -0500

    Se modificio la migracion de borrar schema

--- database/migrations/2026_03_27_163611_drop_visitas_schema.php
+++ database/migrations/2026_03_27_163611_drop_visitas_schema.php
@@ -26,41 +26,51 @@ return new class extends Migration
             DB::table('migrations')
                 ->where('migration', 'like', "%{$schemaName}%")
                 ->orWhere('migration', 'like', "%create_motivos%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_02_13_115244_create_tipo_documentos%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_02_13_122716_create_persona_unos_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_02_13_124535_create_visitas_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_02_18_113659_insert_tipo_documentos_to_tipo_documentos_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_tipo_documentos%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_persona_unos_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_visitas_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%insert_tipo_documentos_to_tipo_documentos_table%") // Ajusta según tus nombres
 
 
 
 
-                ->orWhere('migration', 'like', "%2026_03_10_143813_visitas_historico%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_11_095550_add_columns_to_personas_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%visitas_historico%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_columns_to_personas_table%") // Ajusta según tus nombres
 
-                ->orWhere('migration', 'like', "%2026_03_11_141836_add_soft_deletes_to_tipo_documentos_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_16_171110_create_motivos_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_16_171111_add_soft_deletes_to_motivos_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_17_112437_add_columns_to_visitas.visitas_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_17_112514_add_columns_to_visitas.visitas_historico_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_tipo_documentos_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_motivos_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_motivos_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_columns_to_visitas.visitas_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_columns_to_visitas.visitas_historico_table%") // Ajusta según tus nombres
 
-                ->orWhere('migration', 'like', "%2026_03_20_094123_add_columns_to_visitas.visitas_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_20_112951_create_visitas_trabajadores_ruc_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_20_140236_add_columns_to_visitas.visitas_historico_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_23_155301_add_columns_to_visitas.personas%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_25_100424_create_notifications_table%") // Ajusta según tus nombres
 
-                ->orWhere('migration', 'like', "%2026_03_25_101639_create_imports_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_25_101640_create_exports_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_25_101641_create_failed_import_rows_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_27_103420_add_columns_to_visitas.visitas%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_27_163611_drop_visitas_schema%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_columns_to_visitas.visitas_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_visitas_trabajadores_ruc_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_columns_to_visitas.visitas_historico_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_columns_to_visitas.personas%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_notifications_table%") // Ajusta según tus nombres
 
-                ->orWhere('migration', 'like', "%2026_02_18_141638_insert_regimenes_to_regimenes_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_02_25_153741_add_sede_id_to_users_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_25_101641_create_failed_import_rows_table%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_27_103420_add_columns_to_visitas.visitas%") // Ajusta según tus nombres
-                ->orWhere('migration', 'like', "%2026_03_27_163611_drop_visitas_schema%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_imports_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_exports_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%create_failed_import_rows_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_columns_to_visitas.visitas%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%drop_visitas_schema%") // Ajusta según tus nombres
 
+                ->orWhere('migration', 'like', "%insert_regimenes_to_regimenes_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%insert_clasificaciones_to_clasificaciones_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%insert_sedes_to_sedes_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%insert_areas_to_areas_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%insert_cargos_to_cargos_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%insert_areas_to_areas_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_sede_id_to_users_table%") // Ajusta según tus nombres
+
+                ->orWhere('migration', 'like', "%excel_control1%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_areas_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_cargos_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_clasificaciones_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_regimenes_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_sedes_table%") // Ajusta según tus nombres
+                ->orWhere('migration', 'like', "%add_soft_deletes_to_areas_table%") // Ajusta según tus nombres
                 ->delete();
         });
 
